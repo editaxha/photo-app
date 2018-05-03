888f45120b5f0c28d1faf94f0f211da32d5e9edc
Edit <edit.axha@hotmail.com>
2018-05-03 08:45:04 +0000

    All finish, without amazon s3 production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 Rails.configuration.stripe = {
-  #:publishable_key => 'pk_test_gQEL4ppNlAfWk0ewJEOpaoNk',
-  #:secret_key => 'sk_test_oBmbJzWDnO769iRD3mQEeFHO'
-  :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
-  :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
+  :publishable_key => 'pk_test_gQEL4ppNlAfWk0ewJEOpaoNk',
+  :secret_key => 'sk_test_oBmbJzWDnO769iRD3mQEeFHO'
+  #:publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+  #:secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
